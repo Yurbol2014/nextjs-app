81a95eb7aa6e61642f96aa78a17c3998efcb5665
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2023-01-22 20:01:39 +0300

    первоначальная структура и стили сверстаны

---
+++ pages/burgers/cheeseburger.js
@@ -0,0 +1,11 @@
+
+
+const Cheeseburger=()=>{
+   return(
+      <div>
+         <h1>Чизбургер</h1>
+      </div>
+   )
+}
+
+export default Cheeseburger;
---
+++ pages/burgers/index.js
@@ -0,0 +1,11 @@
+
+
+const Burgers=()=>{
+   return(
+      <div>
+         <h1>Наши бургеры</h1>
+      </div>
+   )
+}
+
+export default Burgers;
