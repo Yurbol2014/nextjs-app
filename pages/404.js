51b2dfea7b46559d609b3048b5e3f5adec4c397c
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2023-01-22 20:59:54 +0300

    добавил стилей и страницу 404

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import { useEffect } from 'react';
+//import { useRouter } from 'next/router';
+
+const NotFoundPage = () => {
+
+   return (
+     <div className="not-found">
+       <h1>Ой...</h1>
+       <h2>Такой страницы здесь нет!</h2>
+       <p>Перехожу на <Link href="/">главную страницу</Link> через 3 секунды...</p>
+     </div>
+   );
+ }
+  
+ export default NotFoundPage;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from 'next/link';
-
+import Link from "next/link";
 
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-   
-      <h1 className={`${styles.title}`}>Главная</h1>
-      <Link href='/burgers'>Все бургеры</Link>
-      
+    <div className={styles.container}>
+      <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
+      <p className={styles.text}>
+        Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное
+        мясо. О других составляющих начинки можно поспорить, ведь это дело
+        вкуса.
+      </p>
+      <p className={styles.text}>
+        Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
+        обслуживания, правильная атмосфера заведения.
+      </p>
+      <Link href="/burgers" className={styles.btn}>Все бургеры</Link>
     </div>
   );
 }
